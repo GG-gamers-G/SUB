d1d94f4185841475eb03be8a5b2439d3706679d0
GG-gamers-G <88082524+GG-gamers-G@users.noreply.github.com>
2021-09-15 19:37:00 +1000

    M

---
+++ js/canon.js
@@ -0,0 +1,20 @@
+class Canon {
+
+
+constructor(x,y,width,height,angle)
+{
+this.x = x
+this.y = y
+this.width = width
+this.height = height
+this.angle = angle
+}
+display(){
+push()
+translate(this.x,this.y)
+rotate(this.angle)
+rect(-10,-20,this.width,this.height)
+pop()
+arc(this.x-40,this.y+80,90,70,PI,TWO_PI,)
+}
+}
